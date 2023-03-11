2bba0d5948cb19591f889405bc4034aa696ae1b6
SergeyMC9730 <bdsservers@yandex.ru>
2023-03-11 21:07:00 +0800

    Loading Circle

---
+++ Classes/LoadingCircle.cpp
@@ -0,0 +1,29 @@
+#include "LoadingCircle.h"
+#include "GameToolbox.h"
+
+bool LoadingCircle::init() {
+    m_pCircle = ax::Sprite::create("loadingCircle.png");
+
+    if(!m_pCircle) {
+        GameToolbox::log("WARN: loadingCircle is not present in your gd textures!");
+    } else {
+        this->addChild(m_pCircle);
+    }
+
+    scheduleUpdate();
+
+    return true;
+}
+void LoadingCircle::update(float delta) {
+    if(m_pCircle) {
+        float rot = m_pCircle->getRotation();
+        rot += delta * 60 * 4;
+        m_pCircle->setRotation(rot);
+    }
+}
+void LoadingCircle::removeMeAndCleanup() {
+    m_pCircle->removeFromParentAndCleanup(true);
+    m_pCircle = nullptr;
+
+    removeFromParentAndCleanup(true);
+}
---
+++ Classes/LoadingCircle.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <axmol.h>
+
+class LoadingCircle : public ax::Layer {
+public:
+    AX_SYNTHESIZE(ax::Sprite *, m_pCircle, Circle);
+
+    bool init();
+    CREATE_FUNC(LoadingCircle);
+
+    void update(float delta);
+    void removeMeAndCleanup();
+};
