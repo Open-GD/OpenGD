a9f3d15e92cfdf0b830d27bd1fb80653b3dc98c1
SaiOwO <70618173+SaiOwO@users.noreply.github.com>
2023-03-23 07:47:23 -0600

    Update MenuItemSpriteExtra.h

--- Classes/MenuItemSpriteExtra.h
+++ Classes/MenuItemSpriteExtra.h
@@ -16,7 +16,11 @@ private:
 	float m_fScaleMult;
 	std::function<void(ax::Node*)> m_fCallback;
 
-public:
+	ax::Vec2 _startPos;
+	ax::Vec2 _offsetPos;
+	bool _altAnim = false;
+
+    public:
 	/// @brief Create a funny scaling button like in gd
 	/// @param sprite The name of the sprite FRAME (it should be in a plist)
 	/// @param callback Runs when you release the button with mouse hovered. Can be a lambda func. The Node* param is the button itself
@@ -24,6 +28,7 @@ public:
 	static MenuItemSpriteExtra* create(const char* sprite, std::function<void(Node*)> callback);
 	static MenuItemSpriteExtra* create(ax::Node* , std::function<void(Node*)> callback);
 	void setScaleMultiplier(float);
+	void setDestination(ax::Vec2 dest);
 	
 	ax::Node* getSprite();
 	void setScale(float) override;
