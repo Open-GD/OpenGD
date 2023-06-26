83c8ac45fe93ffe26c56536663d9abf66f4dd297
Skyue <104967297+Skyue1@users.noreply.github.com>
2023-06-26 14:00:36 +0900

    tutorial layer

---
+++ Source/TutorialLayer.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "2d/CCLayer.h"
+#include "2d/CCNode.h"
+#include "2d/CCLayer.h"
+#include "CCDirector.h"
+#include "2d/CCLabel.h"
+#include "2d/CCMenu.h"
+
+#include "PopupLayer.h"
+
+class TutorialLayer : public PopupLayer
+{
+private:
+	int currentTutorialIndex;
+	ax::Layer* layer1;
+	ax::Label* tap_label;
+	ax::Label* hold_label;
+public:
+	bool init();
+	void onClose();
+	void onNext();
+	static TutorialLayer* create();
+};
